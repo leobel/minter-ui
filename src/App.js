b5b2e7c84b5af019a9b9c165b4eac89fc17166b5
Leobel Izquierdo <leobelizquierdo@gmail.com>
2023-11-02 09:54:36 -0300

    code clean up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 
 import { Wizard, useWizard } from 'react-use-wizard';
-import { createContext, useContext, useEffect, useRef, useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import Offcanvas from 'react-bootstrap/Offcanvas';
-import Dropdown from 'react-bootstrap/Dropdown';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Popover from 'react-bootstrap/Popover';
-import { cborDecode, cborEncode, cborEncodeV2, getAddress } from './utils';
-import Cardano from "./serialization";
 import BuildTx from './components/BuildTx';
 import WalletConnector from './components/WalletConnector';
 import Mint from './components/Mint';
@@ -19,75 +10,30 @@ import CreateScript from './components/CreateScript';
 import SetUp from './components/Setup';
 
 
-const MintContext = createContext({});
 
 function App() {
-  const [wallet, setWallet] = useState(null);
-  const cardanoRef = useRef(null);
-  const apiRef = useRef(null);
-  const supportedWalletsRef = useRef(null);
-  const scriptRef = useRef(null);
-  const tokensRef = useRef([]);
-  const paymentsRef = useRef([]);
-  const collateralsRef = useRef([]);
-  const changeAddressRef = useRef(null);
-  const txInfoRef = useRef(null);
-  const signaturesRef = useRef([]);
-
-  async function loadCardano() {
-    await Cardano.load();
-    const instance = Cardano.instance;
-    cardanoRef.current = instance;
-  }
-
-  const getBalance = async (wallet) => {
-    return cborDecode(await wallet.getBalance());
-  }
-
-  const getChangeAddress = async (wallet) => {
-    const hex = await wallet.getChangeAddress();
-    return getAddress(cardanoRef.current, hex);
-  }
-
-  useEffect(() => {
-    loadCardano();
-  }, []);
-
   return (
-    <MintContext.Provider value={{
-      cardanoRef,
-      apiRef,
-      supportedWalletsRef,
-      scriptRef,
-      tokensRef,
-      paymentsRef,
-      collateralsRef,
-      changeAddressRef,
-      txInfoRef,
-      signaturesRef
-    }}>,
-      <Mint>
-        <div className='container'>
-          <WalletConnector></WalletConnector>
-          <div className="main-content">
-            <div className='row justify-content-center' style={{ height: '100vh' }}>
-              <div style={{ display: 'flex', 'flexDirection': 'column' }}>
-                <Wizard
-                  header={<Header />}
-                  footer={<Footer />}
-                  wrapper={<Wrapper />}
-                >
-                  <SetUp />
-                  <CreateScript />
-                  <BuildTx />
-                  <SignTx />
-                </Wizard>
-              </div>
+    <Mint>
+      <div className='container'>
+        <WalletConnector></WalletConnector>
+        <div className="main-content">
+          <div className='row justify-content-center' style={{ height: '100vh' }}>
+            <div style={{ display: 'flex', 'flexDirection': 'column' }}>
+              <Wizard
+                header={<Header />}
+                footer={<Footer />}
+                wrapper={<Wrapper />}
+              >
+                <SetUp />
+                <CreateScript />
+                <BuildTx />
+                <SignTx />
+              </Wizard>
             </div>
           </div>
         </div>
-      </Mint>
-    </MintContext.Provider>
+      </div>
+    </Mint>
   )
 }
 
